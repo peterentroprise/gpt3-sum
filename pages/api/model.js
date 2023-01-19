b08109915ef5f152d29dc98e30ee87456cb479d8
Zach Weiss <zach@waterstories.com>
2023-01-19 09:35:12 -0500

    asd

--- pages/api/model.js
+++ pages/api/model.js
@@ -2,7 +2,6 @@ import { Configuration, OpenAIApi } from "openai";
 import { jsonc } from "jsonc";
 export default async function handler(req, res) {
   try {
-    //OpenAI API Call
     const configuration = new Configuration({
       apiKey: process.env.OPENAI_API_KEY,
     });
