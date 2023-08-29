22dc29e329a4f42ed4f1399853990d23fef55e81
peterentroprise <67916514+peterentroprise@users.noreply.github.com>
2023-08-29 15:14:30 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,9 @@ const Index = () => {
 
   return (
     <div className="h-screen mx-auto max-w-2xl py-12 px-4">
+       <video width="1200" controls muted="muted">
+            <source src="https://6f95-35-192-44-164.ngrok.io/video" type="video/mp4" />
+        </video>
       <div className=" divide-y-8 py-12 divide-gray-200">
         <p className="text-4xl font-medium py-8">
           Retrieve Media Transformation Demo
