01db6d67339a04e132e41f9d134b4c611053122c
Zach Weiss <zach@waterstories.com>
2023-08-31 11:59:06 -0400

    asd

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import { ImagePrediction } from "../components/ImagePrediction";
 const Index = () => {
 
   return (
-    <div className="h-screen mx-auto max-w-2xl py-12 px-4">
-      <div className=" divide-y-8 py-12 divide-gray-200">
+    <div className="h-screen mx-auto max-w-8xl px-4">
+      <div className=" divide-y-8 divide-gray-200">
         <p className="text-4xl font-medium py-8">
-          Retrieve Colab Prediction Demo
+          Retrieve Colab Demo
         </p>
         <ImagePrediction />
        
